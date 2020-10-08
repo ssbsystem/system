72f6cc7fc871ab90e121cc3e08e0935eb6972e13
Werner Ádám <adamprofi20@gmail.com>
2020-10-08 17:11:02 +0200

    Image object

---
+++ php/Modules/Objects/Image.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * Image
+ */
+class Image
+{
+    private $image;
+    private $url;
+
+    function __construct($url)
+    {
+        $this->url = $url;
+
+        $this->create();
+    }
+
+    function create()
+    {
+        $imgParts = pathinfo($this->url);
+        $extension = $imgParts['extension'];
+
+        // Load image file
+        switch ($extension) {
+            case 'jpg':
+                $this->image = imagecreatefromjpeg($this->url);
+                break;
+            case 'png':
+                $this->image = imagecreatefrompng($this->url);
+                break;
+            case 'bmp':
+                $this->image = imagecreatefrombmp($this->url);
+                break;
+            default:
+                die('Wrong image extentation!');
+                break;
+        }
+    }
+
+    public function scaleImage($maxWidth, $maxHeight)
+    {
+        list($width, $height) = getimagesize($this->image);
+
+        $hRatio = $height / $maxHeight;
+        $wRatio = $width / $maxWidth;
+
+        if ($hRatio > $wRatio) {
+            $newWidth = $width / $hRatio;
+            $newHeight = $maxHeight;
+        } else {
+            $newWidth = $maxWidth;
+            $newHeight = $height / $wRatio;
+        }
+
+        $this->image = imagescale($this->image, $newWidth, $newHeight);
+    }
+
+    public function getBase64File()
+    {
+        $imgParts = pathinfo($this->url);
+        $extension = $imgParts['extension'];
+
+        ob_start();
+
+        switch ($extension) {
+            case 'jpg':
+                imagejpeg($this->image);
+                break;
+            case 'png':
+                imagepng($this->image);
+                break;
+            case 'bmp':
+                imagebmp($this->image);
+                break;
+            default:
+                die('Wrong image extentation!');
+                break;
+        }
+
+        $base64File = ob_get_clean();
+        return $base64File;
+    }
+
+    /**
+     * Get the value of image
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set the value of image
+     *
+     * @return  self
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of url
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set the value of url
+     *
+     * @return  self
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        $this->create();
+
+        return $this;
+    }
+}
--- test.php
+++ test.php
@@ -1,39 +1,60 @@
 <?php
+//stop
+$dirname = "php/uploads/img/desc/t_202-c_202_id-2/1 (1).jpg";/*
+$imageArray = glob($dirname . "*.*");*/
+$j = 0;
 
-$dirname = "php/uploads/img/";
-$imageArray = glob($dirname . "*.*");
+//foreach ($imageArray as $image_name) {
+$image_name = $dirname;
+$imgParts = pathinfo($image_name);
+$imgExtension = $imgParts['extension'];
 
-foreach ($imageArray as $image_name) {
-  $imgParts = pathinfo($image_name);
-  $imgExtension = $imgParts['extension'];
+/* To reserve aspect ratio*/
+list($width, $height) = getimagesize($image_name);
+$newheight = 900;       // Fixed new heigth for every photo in px
+$newwidth = $width / ($height / $newheight);
 
-  /* To reserve aspect ratio*/
-  list($width, $height) = getimagesize($image_name);
-  $newheight = 200;       // Fixed new heigth for every photo in px
-  $newwidth = $width / ($height / $newheight);
-
-  // Load image file
-  switch ($imgExtension) {
-    case 'jpg':
-      $image = imagecreatefromjpeg($image_name);
-      break;
-    case 'png':
-      $image = imagecreatefrompng($image_name);
-      break;
-    case 'bmp':
-      $image = imagecreatefrombmp($image_name);
-      break;
-    default:
-      die('Faild!');
-      break;
-  }
-
-  // Use imagescale() function to scale the image 
-  $img = imagescale($image, $newwidth, $newheight);
-
-  // Output image in the browser 
-  ob_start();
-  imagepng($img);
-  $i = ob_get_clean();
-  echo '<img src="data:image/' . $imgExtension . ';base64,' . base64_encode($i) . '"/>';
+// Load image file
+switch ($imgExtension) {
+  case 'jpg':
+    $image = imagecreatefromjpeg($image_name);
+    break;
+  case 'png':
+    $image = imagecreatefrompng($image_name);
+    break;
+  case 'bmp':
+    $image = imagecreatefrombmp($image_name);
+    break;
+  default:
+    die('Faild!');
+    break;
 }
+
+// Use imagescale() function to scale the image 
+$img = imagescale($image, $newwidth, $newheight);
+
+// Output image in the browser 
+ob_start();
+switch ($imgExtension) {
+  case 'jpg':
+    imagejpeg($img);
+    break;
+  case 'png':
+    imagejpeg($img);
+    break;
+  case 'bmp':
+    imagebmp($img);
+    break;
+  default:
+    die('Faild!');
+    break;
+}
+$i = ob_get_clean();
+
+$targetFileM = "php/uploads/img/mob/t_$j.jpg";
+
+file_put_contents($targetFileM, $i);
+echo '<img src="data:image/' . $imgExtension . ';base64,' . base64_encode($i) . '"/>';
+
+++$j;
+//}
